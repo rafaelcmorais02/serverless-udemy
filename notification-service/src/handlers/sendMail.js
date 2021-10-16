0ca6a7aa2a4fca6af06e7800debabad089fece14
rafaelmoraiscunha <rafael.morais@franq.com.br>
2021-10-16 10:25:41 -0300

    aula 58 - enviando primeiro email

--- notification-service/src/handlers/sendMail.js
+++ notification-service/src/handlers/sendMail.js
@@ -1,7 +1,34 @@
+import AWS from "aws-sdk"
+const ses = new AWS.SES({ region: "sa-east-1" })
+
 async function sendMail(event, context) {
 
-  console.log(event)
-  return event
+  const params = {
+    Source: "rafaelcmorais02@gmail.com",
+    Destination: {
+      ToAddresses: ["rafaelcmorais02@gmail.com"]
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: "Hello Rafael"
+        }
+
+      },
+      Subject: {
+        Data: "Email de Teste"
+      },
+    }
+  }
+
+  try {
+    const result = await ses.sendEmail(params).promise()
+    console.log(result)
+    return result
+  } catch (error) {
+    console.error(error)
+  }
+
 }
 
 export const handler = sendMail;
